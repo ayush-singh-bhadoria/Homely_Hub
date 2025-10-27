aecf8c20a821d5c36d154f4bc5fd79f0eef98dff
ayush-singh-bhadoria <ayushsinghbhadoria111@gmail.com>
2025-10-27 14:45:15 +0530

    2nd commit of my project

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Greeting from './components/Greeting'
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-        <Greeting/>
-        
-    </>
-  )
+    <div className="App">
+      <h1>Let's Start the project</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
