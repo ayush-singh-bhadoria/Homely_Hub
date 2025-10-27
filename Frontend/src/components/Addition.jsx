bb52ccda71d8fad5cc38be69e3b14cb98570fe89
ayush-singh-bhadoria <ayushsinghbhadoria111@gmail.com>
2025-10-27 13:56:43 +0530

    initial commit

---
+++ Frontend/src/components/Addition.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Addition = () => {
+    const num1 = 10
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Addition
---
+++ Frontend/src/components/Greeting.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Greeting = () => {
+  return (
+     <>
+    <h1>Hello Students</h1>
+    </>
+  )
+}
+
+
+export default Greeting
